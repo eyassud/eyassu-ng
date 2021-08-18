dfaddf5f9996138af64ca448d24ff759c4b657e5
dan eyassu <danieleyassu@hotmail.com>
2021-08-18 16:27:52 -0400

    Drop down template created

--- apps/primeng-reactive/src/app/app.module.ts
+++ apps/primeng-reactive/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExamplesModule } from './examples/examples/examples.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ExamplesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/primeng-reactive/src/app/examples/examples/examples.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropDownComponent } from '../drop-down/drop-down.component';
+
+
+
+@NgModule({
+  declarations: [
+    DropDownComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DropDownComponent
+  ]
+})
+export class ExamplesModule { }
